a0c6b3c731853097f49ff98a3a250dbbe3e1e90b
Tomas <rjm1149104294@outlook.com>
2024-01-03 10:02:06 +0800

    Update pngList2Webp.js

--- pngList2Webp.js
+++ pngList2Webp.js
@@ -2,24 +2,31 @@ const { exec } = require('child_process');
 const path = require('path');
 
 function convertImagesToWebP() {
-    const inputPattern = path.join(__dirname, 'xxx', 'xxxxx_%05d.png'); // 输入图片模式
-    const outputGifPath = 'xxx.webp'; // 输出动图路径
+    let qualityArr = [20, 30, 40, 50, 60, 75]
+    // let qualityArr = [20]
+    let frameList = [12, 16, 20, 24]
+    for (let i = 1; i <= 5; i++) {
+        const inputPattern = path.join(__dirname, `xxxxx${i}ys`, '*.png'); // 输入图片模式
+        const outputGifPath = `xxxx${i}ys.webp`; // 输出动图路径
+        let frame = frameList[i - 1]
+        let quality = qualityArr[i - 1]
+        const command = `ffmpeg -i ${inputPattern} -vf \"fps=${frame}\"  -loop 0 -qscale ${quality} -y ${outputGifPath}`;
+        console.log(command)
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing ffmpeg: ${error.message}`);
+                return;
+            }
 
-    const command = `ffmpeg -framerate 60 -i ${inputPattern} -loop 0 -y ${outputGifPath}`;
+            if (stderr) {
+                console.error(`ffmpeg stderr: ${stderr}`);
+                return;
+            }
 
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing ffmpeg: ${error.message}`);
-            return;
-        }
+            console.log('Conversion complete:', stdout);
+        });
+    }
 
-        if (stderr) {
-            console.error(`ffmpeg stderr: ${stderr}`);
-            return;
-        }
-
-        console.log('Conversion complete:', stdout);
-    });
 }
 
 convertImagesToWebP();
